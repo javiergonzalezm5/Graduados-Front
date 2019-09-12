dbab50fd63ce3697e3f85ffd05972376acae5730
javiergonzalezm5 <54966459+javiergonzalezm5@users.noreply.github.com>
2019-09-11 19:06:40 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Exalumnos from './Components/Exalumnos'
+import Routes from './Router'
+
+
+//todo lo que se verá en pantalla
+function App() {
+  return (
+    <div className="App">
+      <Routes/>
+    </div>
+  );
+}
+
+export default App;
